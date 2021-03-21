ca4d97ba9523cd7dba41fdf5ab43d64206b8ceec
Teobotti <61881708+Teobotti@users.noreply.github.com>
2021-03-21 17:41:53 +0100

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,20 @@
+
+import codecs
+
+with open("encrypted.txt", 'rb') as f:
+    encrypted = f.read()
+
+def decrypt(key, encrypted):
+    content = list(' ' * 256)   
+    for i in range(0,256):
+        new_pos = (3**(key+i)) % 257
+        content[i] = (new_pos-1)^i^encrypted[new_pos-1]
+    return bytes(content)  
+
+
+for key in range(1,1000): #provo 1000 chiavi
+        result = decrypt(key, encrypted)
+        flag = codecs.decode(result, errors='ignore')
+        if "{FLG:" in flag: 
+	         print(flag)
+        
